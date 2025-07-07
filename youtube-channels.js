0bf69cae25672a11e30018902ef22621337ddd8d
Balou201 <zikmuytb3@gmail.com>
2025-07-07 19:30:00 +0200

    Create youtube-channels.js

---
+++ youtube-channels.js
@@ -0,0 +1,19 @@
+// youtube-channels.js
+
+const YOUTUBE_CHANNELS = [
+    // --- REMPLACEZ CES PLACEHOLDERS PAR VOS VRAIES URLS DE CHAÎNES YOUTUBE ---
+    'https://www.youtube.com/@mistermv',
+    'https://www.youtube.com/@Squeezie',
+    'https://www.youtube.com/@Amixem',
+    'https://www.youtube.com/@GotagaTV',
+    'https://www.youtube.com/@KametoTV',
+    'https://www.youtube.com/@valentinprg',
+    'https://www.youtube.com/@TiboinShape',
+    'https://www.youtube.com/@Jojol67',
+    'https://www.youtube.com/@Inoxtag',
+    'https://www.youtube.com/@Joyca',
+    'https://www.youtube.com/@FastGoodCuisine',
+    'https://www.youtube.com/@michou',
+    'https://www.youtube.com/@DrWho',
+    // Ajoutez d'autres URLs de chaînes YouTube ici pour avoir plus de variété !
+];
