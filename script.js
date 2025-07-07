f62ea20f7d04c69f2c3585d2dc082bb36151d632
Balou201 <zikmuytb3@gmail.com>
2025-07-07 19:12:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const clickCountDisplay = document.getElementById('clickCountDisplay');
+const buttonTextSpan = document.getElementById('buttonText');
+let clickCount = 0; // Initialisation du compteur
