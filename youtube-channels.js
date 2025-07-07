081b06bfb37ad32cff3aa457e296cc51a5cd8539
Balou201 <zikmuytb3@gmail.com>
2025-07-07 21:14:10 +0200

    Update youtube-channels.js

--- youtube-channels.js
+++ youtube-channels.js
@@ -2,18 +2,12 @@
 
 const YOUTUBE_CHANNELS = [
     // --- REMPLACEZ CES PLACEHOLDERS PAR VOS VRAIES URLS DE CHAÎNES YOUTUBE ---
-    'https://www.youtube.com/@mistermv',
-    'https://www.youtube.com/@Squeezie',
-    'https://www.youtube.com/@Amixem',
-    'https://www.youtube.com/@GotagaTV',
-    'https://www.youtube.com/@KametoTV',
-    'https://www.youtube.com/@valentinprg',
-    'https://www.youtube.com/@TiboinShape',
-    'https://www.youtube.com/@Jojol67',
-    'https://www.youtube.com/@Inoxtag',
-    'https://www.youtube.com/@Joyca',
-    'https://www.youtube.com/@FastGoodCuisine',
-    'https://www.youtube.com/@michou',
-    'https://www.youtube.com/@DrWho',
-    // Ajoutez d'autres URLs de chaînes YouTube ici pour avoir plus de variété !
+    'https://www.youtube.com/@Squeezie', // Exemple : Squeezie (URL réelle)
+    'https://www.youtube.com/@MrBeast',  // Exemple : MrBeast (URL réelle)
+    'https://www.youtube.com/@Micode',   // Exemple : Micode (URL réelle)
+    'https://www.youtube.com/@DocSevenTV', // Exemple : Doc Seven (URL réelle)
+    'https://www.youtube.com/@le_roi_des_rats', // Exemple : Le Roi des Rats (URL réelle)
+    'https://www.youtube.com/@JoueurDuGrenier', // Exemple : JDG (URL réelle)
+    'https://www.youtube.com/@DirtyBiology', // Exemple : DirtyBiology (URL réelle)
+    // Ajoutez ici toutes les URLs réelles de vos chaînes YouTube !
 ];
